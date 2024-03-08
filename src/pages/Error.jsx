165eccc568edc959496467b100ecbbebd8bc2e8a
Shahid Hussain <shahidhussain@Shahids-Air.broadband>
2024-03-08 23:57:05 +0000

    Error component completed

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,22 @@
+import { Link, useRouteError } from "react-router-dom";
+
 function Error() {
-	return <div>Error</div>;
+	const error = useRouteError();
+	console.log(error.status);
+	if (error.status === 404) {
+		return (
+			<main className="grid place-items-center min-h-[100vh]">
+				<h2>Page not found</h2>
+				<p className="text-4xl text-blue-500">404</p>
+				<Link to="/">Back to home page</Link>
+			</main>
+		);
+	}
+	return (
+		<main className="grid place-items-center min-h-[100vh]">
+			<h4 className="text-center font-bold text-4xl"> There was an error</h4>
+		</main>
+	);
 }
 
 export default Error;
