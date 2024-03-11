8fb29ddc1cce39c513d722d994ca5372e0adcfa5
Shahid Hussain <shahidhussain@Shahids-Air.broadband>
2024-03-11 12:14:00 +0000

    About page completed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,19 @@
 function About() {
-	return <div>About</div>;
+	return (
+		<article className="grid place-items-center mt-14">
+			<section className="flex  mb-3 ">
+				<h1 className="text-4xl mr-4">We Love</h1>
+				<div className="stats bg-primary">
+					<div className="stat">
+						<p className="stat-content">Super Store</p>
+					</div>
+				</div>
+			</section>
+			<section>
+				<p className="max-w-[700px]">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla, rerum? Corrupti vitae deserunt consequatur provident rerum reiciendis, architecto reprehenderit asperiores molestiae tempore omnis recusandae, nihil harum quisquam quis ea possimus.</p>
+			</section>
+		</article>
+	);
 }
 
 export default About;
