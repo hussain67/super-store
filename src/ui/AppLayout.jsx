b91cf3b1fddd57628e5270ed11637fcd862eb38e
Shahid Hussain <shahidhussain@Shahids-Air.broadband>
2024-03-17 02:28:23 +0000

    Loading component added

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,16 +1,22 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "./Header";
 import Navbar from "./Navbar";
+import Loading from "./Loading";
 
 function AppLayout() {
+	const navigation = useNavigation();
+	const isLoading = navigation.state === "loading";
 	return (
 		<section>
 			<Header />
 			<Navbar />
-
-			<div className="align-element mt-12">
-				<Outlet />
-			</div>
+			{isLoading ? (
+				<Loading />
+			) : (
+				<div className="align-element mt-12">
+					<Outlet />
+				</div>
+			)}
 		</section>
 	);
 }
