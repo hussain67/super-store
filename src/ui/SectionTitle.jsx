760dc2969301b291424e82599b04cc72b71cc56c
Shahid Hussain <shahidhussain@Shahids-Air.broadband>
2024-04-17 17:52:19 +0100

    Preapered for deployment

--- src/ui/SectionTitle.jsx
+++ src/ui/SectionTitle.jsx
@@ -1,7 +1,7 @@
 function SectionTitle({ text }) {
 	return (
 		<div className=" border-base-50  border-b-[.5px]">
-			<h2 className=" font-medium tracking-wider my-4 text-3xl">{text}</h2>
+			<h2 className=" font-medium tracking-wider my-4 text-3xl capitalize">{text}</h2>
 		</div>
 	);
 }
